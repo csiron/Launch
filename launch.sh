514d10dc03c26668672539c5f670e3f6599329b5
csiron <csiron@hawk.iit.edu>
2015-11-08 21:27:19 -0600

    added user data file line

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-aws ec2 run-instances --image-id $1 --count $2 --instance-type $3  --key-name $4 --security-group-ids $5 --subnet-id $6 --associate-public-ip-address
+aws ec2 run-instances --image-id $1 --count $2 --instance-type $3  --key-name $4 --security-group-ids $5 --subnet-id $6 --user-data file://home/craig/Documents/environment --associate-public-ip-address
 aws wait instance-running
 
 
