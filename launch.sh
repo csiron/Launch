b5dacfa1970e05f23c6fd11e4156a906fc64fc63
csiron <csiron@hawk.iit.edu>
2015-11-08 22:07:06 -0600

    fixed a typo on the subnet for the auto scaling group

--- launch.sh
+++ launch.sh
@@ -9,5 +9,5 @@ aws ec2 wait instance-running --instance-ids ${instancesARR[@]}
 
 aws autoscaling create-launch-configuration --launch-configuration-name csironITMO444auto --image-id ami-d85e75b0 --key-name $4 --security-groups sg-c7c2fea0 --instance-type t1.micro --user-data file://environment/install-env.sh
 
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name csironITMO444autogroup --launch-configuration-name csironITMO444auto --load-balancer-names $2  --health-check-type ELB --min-size 1 --max-size 3 --desired-capacity 2 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier subnet-0aa797d
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name csironITMO444autogroup --launch-configuration-name csironITMO444auto --load-balancer-names $2  --health-check-type ELB --min-size 1 --max-size 3 --desired-capacity 2 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier subnet-0aa7a97d
 
