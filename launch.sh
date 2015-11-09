106bd80e4a2938be4fd05e230a9f8fa207f29fcd
csiron <csiron@hawk.iit.edu>
2015-11-09 00:26:34 -0600

    added echo command for the wait command

--- launch.sh
+++ launch.sh
@@ -4,6 +4,8 @@ declare -a instancesARR
 
 mapfile -t instancesARR < <(aws ec2 run-instances --image-id $1 --count $2 --instance-type $3  --key-name $4 --security-group-ids $5 --subnet-id $6 --user-data file://environment/install-env.sh --associate-public-ip-address --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
 
+echo ${instancesARR[@]}
+
 aws ec2 wait instance-running --instance-ids ${instancesARR[@]} 
 
 #aws elb create-load-balancer --load-balancer-name csironITMO444ELB --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --subnets subnet-0aa7a97d --security-groups sg-c7c2fea0
