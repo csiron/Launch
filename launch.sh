11018941ff79ac6ff7f0ddfdbb34f54035367341
csiron <csiron@hawk.iit.edu>
2015-11-09 00:23:37 -0600

    fixed prior wait command

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 
 declare -a instancesARR
 
-mapfile -t instancesARR < <(aws ec2 run-instances --image-id $1 --count $2 --instance-type $3  --key-name $4 --security-group-ids $5 --subnet-id $6 --user-data file://environment/install-env.sh --associate-public-ip-address aws ec2 describe-instances --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
+mapfile -t instancesARR < <(aws ec2 run-instances --image-id $1 --count $2 --instance-type $3  --key-name $4 --security-group-ids $5 --subnet-id $6 --user-data file://environment/install-env.sh --associate-public-ip-address --output table | grep InstanceId | sed "s/|//g" | tr -d ' ' | sed "s/InstanceId//g")
 
 aws ec2 wait instance-running --instance-ids ${instancesARR[@]} 
 
