67f74fbfaeb19c6dd8d9403257c92cfb4f6e05ba
csiron <csiron@hawk.iit.edu>
2015-11-09 00:31:01 -0600

    added lines to attach instances dynamically to the ELB

--- launch.sh
+++ launch.sh
@@ -10,7 +10,10 @@ aws ec2 wait instance-running --instance-ids ${instancesARR[@]}
 
 #aws elb create-load-balancer --load-balancer-name csironITMO444ELB --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --subnets subnet-0aa7a97d --security-groups sg-c7c2fea0
 
-#aws elb configure-health-check --load-balancer-name csironITMO444ELB --health-check Target=HTTP:80/index.php,Interval=30,UnhealthyThreshold=2,HealthyThreshold=2,Timeout=3
+aws elb register-instances-from-load-balancer --load-balancer-name csironITMO444ELB --instances ${instancesARR[@]}
+
+
+aws elb configure-health-check --load-balancer-name csironITMO444ELB --health-check Target=HTTP:80/index.php,Interval=30,UnhealthyThreshold=2,HealthyThreshold=2,Timeout=3
 
 #aws autoscaling create-launch-configuration --launch-configuration-name csironITMO444auto --image-id ami-d85e75b0 --key-name $4 --security-groups sg-c7c2fea0 --instance-type t1.micro --user-data file://environment/install-env.sh
 
