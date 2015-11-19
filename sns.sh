8a43d47026d1f3ea1951c53da53a1277ee1cc474
csiron <csiron@hawk.iit.edu>
2015-11-19 12:16:08 -0600

    added echo command for the wait command

--- sns.sh
+++ sns.sh
@@ -10,6 +10,8 @@ aws sns subscribe --topic-arn $ARN --protocol sms --notification-endpoint 181548
 
 sleep 60 #wait command added to allow time for subscriber to respond to text to confirm subscription
 
+echo "Waiting for one minute to allow the subscriber to respond to the sms text"
+
 aws s3 mb s3://snstest-bucket
 
 aws s3api put-bucket-notification-configuration --bucket snstest-bucket --notification-configuration file://bucketconfig.json
