171165e0a704869a4e512beba5802b55e5e034dd
csiron <csiron@hawk.iit.edu>
2015-11-19 12:23:19 -0600

    reordered the echo and sleep commands

--- sns.sh
+++ sns.sh
@@ -8,9 +8,9 @@ aws sns set-topic-attributes --topic-arn $ARN --attribute-name DisplayName --att
 
 aws sns subscribe --topic-arn $ARN --protocol sms --notification-endpoint 18154822265 
 
-sleep 60 #wait command added to allow time for subscriber to respond to text to confirm subscription
-
 echo "Waiting for one minute to allow the subscriber to respond to the sms text"
+
+sleep 60 #wait command added to allow time for subscriber to respond to text to confirm subscription
 
 aws s3 mb s3://snstest-bucket
 
