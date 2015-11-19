8bc5e79275558aa0ad142522bbc322bfb31d9eb3
csiron <csiron@hawk.iit.edu>
2015-11-19 11:56:09 -0600

    added wait command to the script

--- sns.sh
+++ sns.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 ARN=(`aws sns create-topic --name csironmp2`)
 
 echo "This is the ARN:  $ARN"
@@ -8,3 +7,9 @@ echo "This is the ARN:  $ARN"
 aws sns set-topic-attributes --topic-arn $ARN --attribute-name DisplayName --attribute-value csironmp2
 
 aws sns subscribe --topic-arn $ARN --protocol sms --notification-endpoint 18154822265 
+
+wait 180 #wait command added to allow time for subscriber to respond to text to confirm subscription
+
+
+
+
