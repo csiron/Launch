6f7d6b8127030ff30d41143f60310caa6784ef30
csiron <csiron@hawk.iit.edu>
2015-11-29 20:09:58 -0600

    changed wait timer

--- sns.sh
+++ sns.sh
@@ -16,7 +16,7 @@ sleep 120 #wait command added to allow time for subscriber to respond to text to
 
 aws s3api create-bucket --bucket cjs-sns-testbucket --acl public-read --region us-east-1
 
-echo "Waiting for ten minutes for bucket to be completely initialized"
-sleep 600
+echo "Waiting for two minutes for bucket to be completely initialized"
+sleep 120
 
 aws s3api put-bucket-notification --bucket cjs-sns-testbucket --notification-configuration file://test.json
